22c45d45b6f5e8285239522181094a6ec570b439
Admin <kishoreg@student.tce.edu>
2025-08-11 02:10:16 +0530

    Temporarily allow all origins for CORS

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ console.log('Environment variables:', {
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin:'*',
   credentials: true
 }));
 app.use(express.json());
